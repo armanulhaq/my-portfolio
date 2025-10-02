2a0b3a9cc40a6604d4e7d9c5b2e992ba0290e10f
Arman Ul Haq <armanulhaq10@gmail.com>
2025-10-02 17:17:58 +0530

    add proper metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -14,9 +14,33 @@ const beVietnam = Be_Vietnam_Pro({
     display: "swap",
 });
 
-export const metadata: Metadata = {
+export const metadata = {
     title: "Arman Ul Haq",
-    description: "Portfolio of Arman Ul Haq | Software Engineer",
+    description: "Portfolio of Arman Ul Haq | Full Stack Software Developer",
+    openGraph: {
+        title: "Arman Ul Haq",
+        description:
+            "Portfolio of Arman Ul Haq | Full Stack Software Developer",
+        url: "https://arman-portfolio-website.vercel.app",
+        siteName: "Arman Ul Haq Portfolio",
+        images: [
+            {
+                url: "/website-screenshot.png",
+                width: 1200,
+                height: 630,
+                alt: "Portfolio of Arman Ul Haq",
+            },
+        ],
+        locale: "en_US",
+        type: "website",
+    },
+    twitter: {
+        card: "summary_large_image",
+        title: "Arman Ul Haq",
+        description:
+            "Portfolio of Arman Ul Haq | Full Stack Software Developer",
+        images: ["/website-screenshot.png"],
+    },
 };
 
 export default function RootLayout({
